e955fb6f28f264e9f8a1b2df18784ff4e99b5ec9
Gerhard Lazu <gerhard@lazu.co.uk>
2012-04-19 12:19:32 +0100

    Now handled by oh-my-zsh plugins

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -6,56 +6,20 @@ alias po='popd'
 alias pbc='pbcopy'
 alias pbp='pbpaste'
 
-alias rs='unicorn_rails'
-alias rsd='rails server'
-alias rg='rails generate'
-alias rd='rails destroy'
-alias rp='rails plugin'
-alias ras='rake spec'
-alias rdd='rake db:drop'
-alias rdc='rake db:create'
-alias rdtc='rake db:test:clone'
-alias rdbm='rake db:migrate'
-alias rc='rails console'
-alias devlog='tail -f log/development.log'
-alias rmodapp='rails application ./ -OTJs -m'
-
 # Basic directory operations
 alias ...='cd ../..'
-
-# Super user
-alias bitch,='sudo'
 
 # grep & ack fu
 alias g='grep -in'
 alias a='ack -i'
 
-# Git related
-alias gst='git status'
-alias gco='git checkout'
-alias gpu='git pull'
-alias gup='git pull --rebase'
-alias gf='git fetch'
-alias gp='git push'
-alias gd='git diff -w "$@" | $EDITOR -R -'
-alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gb='git branch'
-alias gba='git branch -a'
-alias gbd='git branch -d'
-alias gcount='git shortlog -sn --no-merges'
-alias gcp='git cherry-pick'
-alias gsi='git submodule init'
-alias gsu='git submodule update'
-
 # Show history
 alias history='fc -l 1'
 
-# List direcory contents
+# List directory contents
 alias lsa='ls -lah'
 alias l='ls -la'
 alias ll='ls -l'
-alias sl=ls # often screw this up
 
 # Gem aliases
 alias gs='gem search'
@@ -63,10 +27,10 @@ alias gsr='gem search -r'
 alias gsrp='gem search -r --pre'
 
 # Bundler aliases
-alias bi="bundle install"
-alias bu="bundle update"
-alias bx="bundle exec"
-alias bp="bundle package"
+#alias bi="bundle install"
+#alias bu="bundle update"
+#alias bx="bundle exec"
+#alias bp="bundle package"
 
 # ZSH
 alias ralias="source ~/.oh-my-zsh/lib/aliases.zsh"
@@ -79,10 +43,6 @@ alias M='mvim'
 # tmux
 alias T='tmux'
 
-# cucumber & rspec
-alias C='CUCUMBER_FORMAT=pretty bx cucumber -r features'
-alias R='bx rspec'
-
+# misc
 alias x='extract'
-
 alias shasum256='openssl dgst -sha256'
--- lib/functions.zsh
+++ lib/functions.zsh
@@ -15,71 +15,12 @@ function take() {
   cd $1
 }
 
-function extract() {
-    unset REMOVE_ARCHIVE
-    if test "$1" = "-r"; then
-        REMOVE=1
-        shift
-    fi
-  if [[ -f $1 ]]; then
-    case $1 in
-      *.tar.bz2) tar xvjf $1;;
-      *.tar.gz) tar xvzf $1;;
-      *.tar.xz) tar xvJf $1;;
-      *.tar.lzma) tar --lzma -xvf $1;;
-      *.bz2) bunzip $1;;
-      *.rar) unrar $1;;
-      *.gz) gunzip $1;;
-      *.tar) tar xvf $1;;
-      *.tbz2) tar xvjf $1;;
-      *.tgz) tar xvzf $1;;
-      *.zip) unzip $1;;
-      *.Z) uncompress $1;;
-      *.7z) 7z x $1;;
-      *) echo "'$1' cannot be extracted via >extract<";;
-    esac
-
-    if [[ $REMOVE_ARCHIVE -eq 1 ]]; then
-        echo removing "$1";
-        /bin/rm "$1";
-    fi
-
-  else
-    echo "'$1' is not a valid file"
-  fi
-}
-
 function grsync {
   rsync --verbose --recursive --append --copy-links --human-readable --progress --bwlimit=$1 $2 .
-}
-
-function runrar {
-  # if [ $1 ]; then pattern=$1; else pattern=**/*.rar; fi
-  for f (**/*.rar) { unrar e $f }
 }
 
 function catalog {
   catalog_path=$1
   catalog_name=$catalog_path[(ws:/:)-1]
   tree -D -h $catalog_path > "$catalog_name.txt"
-}
-
-function whitespace {
-  find . -name '*.rb' -exec sed -i '' 's/ *$//g' {} \;
-}
-
-function mongo_reload {
-  echo "Started..."
-  mongo_version=$(brew info mongo | head -1 | cut -d' ' -f 2)
-  mongo_lock=/usr/local/var/mongodb/mongod.lock
-
-  # if [ ps aux | grep [m]ongo ]; then
-  #   echo "Stopping mongo"
-  #   launchctl unload -w $HOME/Library/LaunchAgents/org.mongodb.mongod.plist
-  # fi
-  # if [ -f $mongo_lock ]; then
-  #   rm $mongo_lock
-  # fi
-  # mongod run --repair --config /usr/local/Cellar/mongodb/$mongo_version/mongod.conf
-  # launchctl load -w $HOME/Library/LaunchAgents/org.mongodb.mongod.plist
 }
